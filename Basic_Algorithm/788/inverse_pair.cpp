f6b4458b038e9d7eea45f8449622d0503789ffae
Zhang-Yukun <2936501245@qq.com>
2025-04-27 23:55:58 +0800

    Refinish 788 in cpp

--- Basic_Algorithm/788/inverse_pair.cpp
+++ Basic_Algorithm/788/inverse_pair.cpp
@@ -1,42 +1,39 @@
 #include <iostream>
-
 using namespace std;
 
 const int N = 1e5 + 10;
-int m[N], t[N], n;
+int n, a[N], t[N];
 long long res;
 
-void merge_sort(int m[], int left, int right);
+void merge_sort(int a[], int l, int r);
 
 int main() {
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &m[i]);
-    }
-    merge_sort(m, 0, n - 1);
-    printf("%lld", res);
-    return 0;
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++)
+    scanf("%d", &a[i]);
+  merge_sort(a, 0, n - 1);
+  printf("%lld", res);
+  return 0;
 }
 
-void merge_sort(int m[], int left, int right) {
-    if (left >= right) {
-        return;
+void merge_sort(int a[], int l, int r) {
+  if (l >= r)
+    return;
+  int m = l + ((r - l) >> 1), i = l, j = m + 1, k = 0;
+  merge_sort(a, l, m);
+  merge_sort(a, m + 1, r);
+  while (i <= m && j <= r) {
+    if (a[i] <= a[j])
+      t[k++] = a[i++];
+    else {
+      t[k++] = a[j++];
+      res += (m - i + 1);
     }
-    int middle = left + ((right - left) >> 1);
-    merge_sort(m, left, middle);
-    merge_sort(m, middle + 1, right);
-    int i = left, j = middle + 1, k = 0;
-    while ((i <= middle) && (j <= right)) {
-        if (m[i] <= m[j]) {
-            t[k++] = m[i++];
-        } else {
-            t[k++] = m[j++];
-            res += (middle  - i + 1);
-        }
-    }
-    while (i <= middle) t[k++] = m[i++];
-    while (j <= right) t[k++] = m[j++];
-    for (int i = 0; i < k; i++) {
-        m[left + i] = t[i];
-    }
+  }
+  while (i <= m)
+    t[k++] = a[i++];
+  while (j <= r)
+    t[k++] = a[j++];
+  for (i = l, k = 0; i <= r; i++, k++)
+    a[i] = t[k];
 }
