75c8d8ebbe10d4c26fb003a8aac356897e17c864
Zhang-Yukun <2936501245@qq.com>
2025-04-28 22:32:57 +0800

    Refinish 789 in cpp

--- Basic_Algorithm/789/num_range.cpp
+++ Basic_Algorithm/789/num_range.cpp
@@ -1,55 +1,44 @@
 #include <iostream>
-
 using namespace std;
 
 const int N = 1e5 + 10;
+int n, q, k, a[N];
 
-int a[N], n, q, k, res[2];
-
-int binary_search_left(int left, int right, int k);
-int binary_search_right(int left, int right, int k);
-
+int binary_search_left(int a[], int l, int r, int k);
+int binary_search_right(int a[], int l, int r, int k);
 
 int main() {
-    scanf("%d %d", &n, &q);
-    for (int i = 0; i < n; i++) scanf("%d", &a[i]);
-    for (int i = 0; i < q; i++) {
-        scanf("%d", &k);
-        res[0] = binary_search_left(0, n - 1, k);
-        res[1] = binary_search_right(0, n - 1, k);
-        printf("%d %d\n", res[0], res[1]);
-    }
-    return 0;
+  scanf("%d%d", &n, &q);
+  for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+  while (q--) {
+    scanf("%d", &k);
+    printf("%d %d\n", binary_search_left(a, 0, n - 1, k), binary_search_right(a, 0, n - 1, k));
+  }
+  return 0;
 }
 
-int binary_search_left(int left, int right, int k) {
-    int l = left, r = right, m = l + ((r - l) >> 1);
-    while (l <= r) {
-        if  (a[m] == k) {
-            if ((m == 0) || (a[m - 1] < k)) return m;
-            r = m - 1;
-        } else if (a[m] < k) {
-            l = m + 1;
-        } else {
-            r = m - 1;
-        }
-        m = l + ((r - l) >> 1);
-    }
-    return -1;
+int binary_search_left(int a[], int l, int r, int k) {
+  int m = l + ((r - l) >> 1);
+  while (l <= r) {
+    if (a[m] == k) {
+      if (m == 0 || a[m - 1] != k) return m;
+      else r = m - 1;
+    } else if (a[m] < k) l = m + 1;
+    else r = m - 1;
+    m = l + ((r - l) >> 1);
+  }
+  return -1;
 }
 
-int binary_search_right(int left, int right, int k) {
-    int l = left, r = right, m = l + ((r - l) >> 1);
-    while (l <= r) {
-        if (a[m] == k) {
-            if ((m == n - 1) || (a[m + 1] > k)) return m;
-            l = m + 1;
-        } else if (a[m] < k) {
-            l = m + 1;
-        } else {
-            r = m - 1;
-        }
-        m = l + ((r - l) >> 1);
-    }
-    return -1;
+int binary_search_right(int a[], int l, int r, int k) {
+  int m = l + ((r - l) >> 1);
+  while (l <= r) {
+    if (a[m] == k) {
+      if (m == n - 1 || a[m + 1] != k) return m;
+      else l = m + 1;
+    } else if (a[m] < k) l = m + 1;
+    else r = m - 1;
+    m = l + ((r - l) >> 1);
+  }
+  return -1;
 }
