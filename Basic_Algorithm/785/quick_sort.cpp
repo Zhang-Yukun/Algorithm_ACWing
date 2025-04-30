b9313bd8192dd1f5757e8d78ab675ea265c281d5
Zhang-Yukun <2936501245@qq.com>
2025-04-30 23:13:46 +0800

    Refinish 785, 786 in cpp

--- Basic_Algorithm/785/quick_sort.cpp
+++ Basic_Algorithm/785/quick_sort.cpp
@@ -5,24 +5,27 @@ using namespace std;
 const int N = 1e5 + 10;
 int n, q[N];
 
-void quick_sort(int q[], int l, int r);
+void quick_sort(int a[], int l, int r);
 
 int main() {
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) scanf("%d", &q[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &q[i]);
+    }
     quick_sort(q, 0, n - 1);
     for (int i = 0; i < n; i++) printf("%d ", q[i]);
+    return 0;
 }
 
-void quick_sort(int q[], int l, int r) {
+void quick_sort(int a[], int l, int r) {
     if (l >= r) return;
-    int i = l, j = r, p = q[l + ((r - l) >> 1)];
+    int i = l, j = r, pivot = a[(i + j) >> 1];
     while (true) {
-        while (q[i] < p) i++;
-        while (q[j] > p) j--;
+        while (a[i] < pivot) i++;
+        while (a[j] > pivot) j--;
         if (i >= j) break;
-        swap(q[i++], q[j--]);
+        swap(a[i++], a[j--]);
     }
-    quick_sort(q, l, j);
-    quick_sort(q, j + 1, r);
+    quick_sort(a, l, j);
+    quick_sort(a, j + 1, r);
 }
--- Basic_Algorithm/786/the_k_number.cpp
+++ Basic_Algorithm/786/the_k_number.cpp
@@ -4,22 +4,24 @@ using namespace std;
 const int N = 1e5 + 10;
 int n, k, q[N];
 
-int quick_sort(int q[], int l, int r, int k);
+int quick_sort(int a[], int l, int r, int k);
 
 int main() {
     scanf("%d%d", &n, &k);
     for (int i = 0; i < n; i++) scanf("%d", &q[i]);
-    printf("%d", quick_sort(q, 0, n - 1, k - 1));
+    printf("%d", quick_sort(q, 0, n - 1, k));
+    return 0;
 }
 
-int quick_sort(int q[], int l, int r, int k) {
-    if (l >= r) return q[k];
-    int i = l - 1, j = r + 1, p = q[l + ((r - l) >> 1)];
-    while (i < j) {
-        while (q[++i] < p);
-        while (q[--j] > p);
-        if (i < j) swap(q[i], q[j]);
+int quick_sort(int a[], int l, int r, int k) {
+    if (l >= r) return a[l];
+    int i = l, j = r, pivot = a[(i + j) >> 1];
+    while (true) {
+        while (a[i] < pivot) i++;
+        while (a[j] > pivot) j--;
+        if (i >= j) break;
+        swap(a[i++], a[j--]);
     }
-    if (k <= j) return quick_sort(q, l, j, k);
-    else return quick_sort(q, j + 1, r, k);
+    if (k <= j - l + 1) return quick_sort(a, l, j, k);
+    else return quick_sort(a, j + 1, r, k - j + l - 1);
 }
